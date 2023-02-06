4686ebe3c74aa11a2d7cdedc64879c16e53505b5
Adebara Ayomide <adebaraayomide@gmail.com>
2023-02-06 13:46:27 +0100

    Ayomide: init code

---
+++ contracts/insure.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract insure {
+
+
+ // Insurance providers 
+ // 1. function to create a platform 
+ // 2. function to add deposit into it for the cover
+ // 3. function to set the percentage 
+ 
+ // admins fuction 
+ // function to approve a particular protocol submited 
+ 
+ // Users function 
+ // function to chose an insurance 
+ // function to set the date to insure  
+
+    function createNewInsure (string memory protocolName, uint percentage) public {
+
+    }
+
+    function depositIntoInsureVault () public {
+
+    }
+
+    function setercentage () public {
+        
+    }
+}
